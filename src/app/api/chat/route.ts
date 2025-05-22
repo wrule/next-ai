59b72ae3033dc2e8e77e4abfe8c4600158c85a63
jimao <shihao.gu@perfma.com>
2025-05-22 23:56:36 +0800

    feat: 最简化的转换

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,47 +2,20 @@ import { mastra } from '@/mastra';
 import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
-  let timer: NodeJS.Timeout;
-  const encoder = new TextEncoder();
-  const decoder = new TextDecoder('utf-8');
-
   const { searchParams } = request.nextUrl;
   const agent = mastra.getAgent('BlankAgent');
-  const reader = (await agent.stream(searchParams.get('query') ?? '你好'))
-    .toDataStream()
-    .getReader();
+  const vercelStream = (await agent.stream(searchParams.get('query') ?? '你好')).toDataStream();
 
-  while (true) {
-    const a = await reader.read();
-    const b = a.value;
-    if (b) {
-      const decoder = new TextDecoder('utf-8');
-      console.log(decoder.decode(b));
-    }
-    if (a.done) {
-      break;
-    }
-  }
-
-  const responseStream = new ReadableStream({
-    start: (controller) => {
-      let count = 0;
-      timer = setInterval(() => {
-        if (count < 100) {
-          controller.enqueue(encoder.encode('你好，世界\n'));
-        } else {
-          controller.enqueue(encoder.encode('结束\n'));
-          controller.close();
-          clearInterval(timer);
-        }
-        count++;
-      }, 100);
-    },
-    cancel: (reason?: any) => {
-      clearInterval(timer);
+  const decoder = new TextDecoder('utf-8');
+  const openAiTransform = new TransformStream({
+    transform: (chunk, controller) => {
+      const text = decoder.decode(chunk);
+      console.log(text);
+      controller.enqueue(text);
     },
   });
-  return new Response(responseStream, {
+
+  return new Response(vercelStream.pipeThrough(openAiTransform), {
     headers: {
       'Content-Type': 'text/event-stream; charset=utf-8',
       'Cache-Control': 'no-cache, no-store, must-revalidate',
@@ -50,6 +23,4 @@ export async function GET(request: NextRequest) {
       'X-Content-Type-Options': 'nosniff',
     },
   });
-
-  return result.toDataStreamResponse();
 }

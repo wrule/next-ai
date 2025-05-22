8326ff5c9d8c6551820b951e84b16802cc125bad
jimao <shihao.gu@perfma.com>
2025-05-22 22:11:20 +0800

    feat: 准备用setTimeout实现

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,26 +2,28 @@ import { mastra } from '@/mastra';
 import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
-  // const { searchParams } = request.nextUrl;
-  // const agent = mastra.getAgent('BlankAgent');
-  // const result = await agent.stream(searchParams.get('query') ?? '你好');
-
-  // const dataStream = result.toDataStream();
-  // const reader = dataStream.getReader();
-  // while (true) {
-  //   const a = await reader.read();
-  //   const b = a.value;
-  //   if (b) {
-  //     const decoder = new TextDecoder('utf-8');
-  //     console.log(decoder.decode(b));
-  //   }
-  //   if (a.done) {
-  //     break;
-  //   }
-  // }
-
-  const encoder = new TextEncoder();
   let timer: NodeJS.Timeout;
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder('utf-8');
+
+  const { searchParams } = request.nextUrl;
+  const agent = mastra.getAgent('BlankAgent');
+  const reader = (await agent.stream(searchParams.get('query') ?? '你好'))
+    .toDataStream()
+    .getReader();
+
+  while (true) {
+    const a = await reader.read();
+    const b = a.value;
+    if (b) {
+      const decoder = new TextDecoder('utf-8');
+      console.log(decoder.decode(b));
+    }
+    if (a.done) {
+      break;
+    }
+  }
+
   const responseStream = new ReadableStream({
     start: (controller) => {
       let count = 0;

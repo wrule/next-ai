fc494b9f472db51f55d35995b35df4658c0f8fa3
jimao <shihao.gu@perfma.com>
2025-05-23 00:38:22 +0800

    feat: 删除不需要的代码

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,25 +2,6 @@ import { mastra } from '@/mastra';
 import { NextRequest } from 'next/server';
 import openaiStream from '@/app/utils/openaiStream';
 
-const chatCompletionChunk = (uid: string, content: string) => {
-  return 'data: ' + JSON.stringify({
-    id: uid,
-    provider: 'agent',
-    model: 'agent',
-    object: 'chat.completion.chunk',
-    created: Math.floor(Date.now() / 1000),
-    choices: [
-      {
-        index: 0,
-        delta: {
-          role: 'assistant',
-          content,
-        },
-      },
-    ],
-  }) + '\n\n';
-}
-
 export async function GET(request: NextRequest) {
   const { searchParams } = request.nextUrl;
   const agent = mastra.getAgent('BlankAgent');

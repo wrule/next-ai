e9912741db6f06d1adb914b7e6b03717e883da70
jimao <shihao.gu@perfma.com>
2025-05-22 20:07:38 +0800

    feat: 解析流的代码

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -5,5 +5,19 @@ export async function GET(request: NextRequest) {
   const { searchParams } = request.nextUrl;
   const agent = mastra.getAgent('BlankAgent');
   const result = await agent.stream(searchParams.get('query') ?? '你好');
+
+  const dataStream = result.toDataStream();
+  const reader = dataStream.getReader();
+  while (true) {
+    const a = await reader.read();
+    const b = a.value;
+    if (b) {
+      const decoder = new TextDecoder('utf-8');
+      console.log(decoder.decode(b));
+    }
+    if (a.done) {
+      break;
+    }
+  }
   return result.toDataStreamResponse();
 }

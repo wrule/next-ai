008331450d720cfd301498ed41e54a95285572e1
jimao <shihao.gu@perfma.com>
2025-05-23 00:42:52 +0800

    feat: 开启openai兼容模式

---
+++ src/app/api/agent/[agentName]/v1/chat/completions/route.ts
@@ -0,0 +1,17 @@
+import { mastra } from '@/mastra';
+import { NextRequest } from 'next/server';
+import openaiStream from '@/app/utils/openaiStream';
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
+  const agent = mastra.getAgent('BlankAgent');
+  const vercelStream = (await agent.stream(searchParams.get('query') ?? '你好')).toDataStream();
+  return new Response(openaiStream(vercelStream), {
+    headers: {
+      'Content-Type': 'text/event-stream; charset=utf-8',
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      'Connection': 'keep-alive',
+      'X-Content-Type-Options': 'nosniff',
+    },
+  });
+}

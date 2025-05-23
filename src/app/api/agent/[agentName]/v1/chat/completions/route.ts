4ef19e846a411e4e38f30e49bf37c364459cd741
jimao <shihao.gu@perfma.com>
2025-05-24 03:38:18 +0800

    feat: post方式

--- src/app/api/agent/[agentName]/v1/chat/completions/route.ts
+++ src/app/api/agent/[agentName]/v1/chat/completions/route.ts
@@ -15,3 +15,17 @@ export async function GET(request: NextRequest) {
     },
   });
 }
+
+export async function POST(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
+  const agent = mastra.getAgent('BlankAgent');
+  const vercelStream = (await agent.stream(searchParams.get('query') ?? '你好')).toDataStream();
+  return new Response(openaiStream(vercelStream), {
+    headers: {
+      'Content-Type': 'text/event-stream; charset=utf-8',
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      'Connection': 'keep-alive',
+      'X-Content-Type-Options': 'nosniff',
+    },
+  });
+}

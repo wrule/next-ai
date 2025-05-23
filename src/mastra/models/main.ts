9b9a81878aeb5a6f6fa7bea3f2a6acd1e55a14da
jimao <shihao.gu@perfma.com>
2025-05-24 02:42:47 +0800

    feat: 更换模型

--- src/mastra/models/main.ts
+++ src/mastra/models/main.ts
@@ -1,8 +1,21 @@
+import { createQwen } from 'qwen-ai-provider';
 import { createOpenRouter } from '@openrouter/ai-sdk-provider';
+import { createOpenAICompatible } from '@ai-sdk/openai-compatible';
 
-const provider = createOpenRouter({
+// const provider = createOpenRouter({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+const provider = createQwen({
+  baseURL: process.env.BASE_URL!,
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+// const provider = createOpenAICompatible({
+//   name: 'provider',
+//   baseURL: process.env.BASE_URL!,
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
 const main_model = provider(process.env.MODEL_NAME!);
 

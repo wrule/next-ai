5165d15c4ecce4766b9ee1f459977201a83158cc
jimao <shihao.gu@perfma.com>
2025-05-24 03:00:17 +0800

    feat: change provider

--- src/mastra/models/main.ts
+++ src/mastra/models/main.ts
@@ -6,16 +6,16 @@ import { createOpenAICompatible } from '@ai-sdk/openai-compatible';
 //   apiKey: process.env.OPENAI_API_KEY,
 // });
 
-const provider = createQwen({
-  baseURL: process.env.BASE_URL!,
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-// const provider = createOpenAICompatible({
-//   name: 'provider',
+// const provider = createQwen({
 //   baseURL: process.env.BASE_URL!,
 //   apiKey: process.env.OPENAI_API_KEY,
 // });
+
+const provider = createOpenAICompatible({
+  name: 'provider',
+  baseURL: process.env.BASE_URL!,
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 const main_model = provider(process.env.MODEL_NAME!);
 

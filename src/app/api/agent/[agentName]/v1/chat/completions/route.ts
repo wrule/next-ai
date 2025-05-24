25f39c1661b77c431d9b0a0f5d501489aadd8d73
jimao <shihao.gu@perfma.com>
2025-05-24 16:37:32 +0800

    feat: 改造get方法中

--- src/app/api/agent/[agentName]/v1/chat/completions/route.ts
+++ src/app/api/agent/[agentName]/v1/chat/completions/route.ts
@@ -2,21 +2,38 @@ import { mastra } from '@/mastra';
 import { NextRequest, NextResponse } from 'next/server';
 import openaiStream from '@/app/utils/openaiStream';
 
+const paramsCollector = async (request: NextRequest) => {
+  if (request.method === 'GET') {
+    return Object.fromEntries(request.nextUrl.searchParams);
+  } else {
+    return await request.json();
+  }
+}
+
 export async function GET(
   request: NextRequest,
   { params }: { params: { agentName: Parameters<typeof mastra.getAgent>[0] } },
 ) {
-  const { searchParams } = request.nextUrl;
   const agent = mastra.getAgent(params.agentName);
-  const vercelStream = (await agent.stream(searchParams.get('query') ?? '你好')).toDataStream();
-  return new Response(openaiStream(vercelStream), {
-    headers: {
-      'Content-Type': 'text/event-stream; charset=utf-8',
-      'Cache-Control': 'no-cache, no-store, must-revalidate',
-      'Connection': 'keep-alive',
-      'X-Content-Type-Options': 'nosniff',
-    },
-  });
+  const requestParams = await paramsCollector(request);
+  const query = requestParams.query || 'hello';
+  delete requestParams.query;
+  const stream = requestParams.stream !== 'false';
+  delete requestParams.stream;
+  if (stream) {
+    const vercelStream = (await agent.stream(query, requestParams)).toDataStream();
+    return new Response(openaiStream(vercelStream), {
+      headers: {
+        'Content-Type': 'text/event-stream; charset=utf-8',
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Connection': 'keep-alive',
+        'X-Content-Type-Options': 'nosniff',
+      },
+    });
+  } else {
+    const { text, usage } = await agent.generate(query, requestParams);
+    return NextResponse.json({ text, usage });
+  }
 }
 
 export async function POST(request: NextRequest) {

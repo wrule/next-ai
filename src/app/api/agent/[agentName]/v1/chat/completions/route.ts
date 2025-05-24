361460023eeb96b5d587b6e46150c086ce57e088
jimao <shihao.gu@perfma.com>
2025-05-25 00:19:57 +0800

    feat: 准备优化代码

--- src/app/api/agent/[agentName]/v1/chat/completions/route.ts
+++ src/app/api/agent/[agentName]/v1/chat/completions/route.ts
@@ -1,6 +1,7 @@
 import { mastra } from '@/mastra';
 import { NextRequest, NextResponse } from 'next/server';
 import openaiStream, { chatCompletion } from '@/app/utils/openaiStream';
+import { Agent } from '@mastra/core';
 
 const sseHeaders = {
   'Content-Type': 'text/event-stream; charset=utf-8',
@@ -8,6 +9,17 @@ const sseHeaders = {
   'Connection': 'keep-alive',
   'X-Content-Type-Options': 'nosniff',
 };
+
+const a = async (agent: Agent, stream: boolean, messages: any, requestParams: any) => {
+  const uid = crypto.randomUUID();
+  if (stream) {
+    const vercelStream = (await agent.stream(messages, requestParams)).toDataStream();
+    return new Response(openaiStream(uid, vercelStream), { headers: sseHeaders });
+  } else {
+    const { text: content } = await agent.generate(messages, requestParams);
+    return NextResponse.json(chatCompletion(uid, content));
+  }
+}
 
 export async function GET(
   request: NextRequest,

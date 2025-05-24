be8c0c100fdd537e196848e13761cf561379ebd8
jimao <shihao.gu@perfma.com>
2025-05-24 15:56:19 +0800

    feat: dify初步调通

--- src/app/api/agent/[agentName]/v1/chat/completions/route.ts
+++ src/app/api/agent/[agentName]/v1/chat/completions/route.ts
@@ -1,5 +1,5 @@
 import { mastra } from '@/mastra';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import openaiStream from '@/app/utils/openaiStream';
 
 export async function GET(request: NextRequest) {
@@ -18,16 +18,22 @@ export async function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  console.log(1234, body);
-  const { searchParams } = request.nextUrl;
-  const agent = mastra.getAgent('BlankAgent');
-  const vercelStream = (await agent.stream(searchParams.get('query') ?? '你好')).toDataStream();
-  return new Response(openaiStream(vercelStream), {
-    headers: {
-      'Content-Type': 'text/event-stream; charset=utf-8',
-      'Cache-Control': 'no-cache, no-store, must-revalidate',
-      'Connection': 'keep-alive',
-      'X-Content-Type-Options': 'nosniff',
-    },
-  });
+  console.log(body);
+  if (body.stream) {
+    const { searchParams } = request.nextUrl;
+    const agent = mastra.getAgent('BlankAgent');
+    const vercelStream = (await agent.stream(searchParams.get('query') ?? '你好')).toDataStream();
+    return new Response(openaiStream(vercelStream), {
+      headers: {
+        'Content-Type': 'text/event-stream; charset=utf-8',
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Connection': 'keep-alive',
+        'X-Content-Type-Options': 'nosniff',
+      },
+    });
+  } else {
+    return NextResponse.json({
+
+    });
+  }
 }

05b78b56b2ae834ea23975b08884a583e3ee39c8
jimao <shihao.gu@perfma.com>
2025-05-24 23:54:33 +0800

    feat: 错误处理

--- src/app/api/agent/[agentName]/v1/chat/completions/route.ts
+++ src/app/api/agent/[agentName]/v1/chat/completions/route.ts
@@ -21,19 +21,26 @@ export async function GET(
   request: NextRequest,
   { params }: { params: { agentName: Parameters<typeof mastra.getAgent>[0] } },
 ) {
-  const uid = crypto.randomUUID();
-  const agent = mastra.getAgent(params.agentName);
-  const requestParams = await paramsCollector(request);
-  const query = requestParams.query || 'hello';
-  delete requestParams.query;
-  const stream = requestParams.stream !== 'false';
-  delete requestParams.stream;
-  if (stream) {
-    const vercelStream = (await agent.stream(query, requestParams)).toDataStream();
-    return new Response(openaiStream(uid, vercelStream), { headers: sseHeaders });
-  } else {
-    const { text: content } = await agent.generate(query, requestParams);
-    return NextResponse.json(chatCompletion(uid, content));
+  try {
+    const uid = crypto.randomUUID();
+    const agent = mastra.getAgent(params.agentName);
+    const requestParams = await paramsCollector(request);
+    const query = requestParams.query || 'hello';
+    delete requestParams.query;
+    const stream = requestParams.stream !== 'false';
+    delete requestParams.stream;
+    if (stream) {
+      const vercelStream = (await agent.stream(query, requestParams)).toDataStream();
+      return new Response(openaiStream(uid, vercelStream), { headers: sseHeaders });
+    } else {
+      const { text: content } = await agent.generate(query, requestParams);
+      return NextResponse.json(chatCompletion(uid, content));
+    }
+  } catch (error: any) {
+    return NextResponse.json({
+      success: false,
+      message: error?.message,
+    }, { status: 500 });
   }
 }
 

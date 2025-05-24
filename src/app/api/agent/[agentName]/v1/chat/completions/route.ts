b9a685724ec6bba78257c65ccdb165e6bec8426b
jimao <shihao.gu@perfma.com>
2025-05-24 17:00:09 +0800

    feat: 代码复制过来

--- src/app/api/agent/[agentName]/v1/chat/completions/route.ts
+++ src/app/api/agent/[agentName]/v1/chat/completions/route.ts
@@ -37,14 +37,20 @@ export async function GET(
   }
 }
 
-export async function POST(request: NextRequest) {
-  const body = await request.json();
-  console.log(body);
-  if (body.stream) {
-    const { searchParams } = request.nextUrl;
-    const agent = mastra.getAgent('BlankAgent');
-    const vercelStream = (await agent.stream(searchParams.get('query') ?? '你好')).toDataStream();
-    return new Response(openaiStream(vercelStream), {
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { agentName: Parameters<typeof mastra.getAgent>[0] } },
+) {
+  const uid = crypto.randomUUID();
+  const agent = mastra.getAgent(params.agentName);
+  const requestParams = await paramsCollector(request);
+  const query = requestParams.query || 'hello';
+  delete requestParams.query;
+  const stream = requestParams.stream !== 'false';
+  delete requestParams.stream;
+  if (stream) {
+    const vercelStream = (await agent.stream(query, requestParams)).toDataStream();
+    return new Response(openaiStream(uid, vercelStream), {
       headers: {
         'Content-Type': 'text/event-stream; charset=utf-8',
         'Cache-Control': 'no-cache, no-store, must-revalidate',
@@ -53,8 +59,7 @@ export async function POST(request: NextRequest) {
       },
     });
   } else {
-    return NextResponse.json({
-
-    });
+    const { text: content } = await agent.generate(query, requestParams);
+    return NextResponse.json(chatCompletion(uid, content));
   }
 }
